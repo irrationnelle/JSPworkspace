569050dc081b5e98ded8e4ff083de8199a2a3607
irrationnelle <drakkarverenis@gmail.com>
2016-10-14 10:22:33 +0900

    01 1st commit(raw java and web.xml)

---
+++ Day02/src/servlet/TestServlet.java
@@ -0,0 +1,33 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter writer = response.getWriter();
+		
+		writer.println("<html>");
+		writer.println("<head>");
+		
+		writer.println("<title> 자바로 만든 서블릿</title>");
+		
+		writer.println("</head>");
+		
+		writer.println("<body>");
+		writer.println("이 화면은 자바로 작성된 서블릿이 만들어준");
+		writer.println("html 문서입니다. <br>");
+		writer.println("현재 시각:" + new Date());
+		writer.println("</body>");
+		
+		writer.println("</html>");
+		
+	}
+}
