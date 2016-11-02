148f6e406a56a1197177c84cc224dd6d43ee9701
irrationnelle <drakkarverenis@gmail.com>
2016-11-03 01:04:55 +0900

    01 exercise MVC pattern and JSTL 161103

--- BootstrapBoard05/WebContent/board_list.jsp
+++ BootstrapBoard05/WebContent/board_list.jsp
@@ -4,6 +4,8 @@
 <%@page import="service.BoardService"%>
 <%@ page language="java" contentType="text/html; charset=EUC-KR"
     pageEncoding="EUC-KR"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,18 +13,6 @@
 </head>
 
 <body>
-<%
-	int currentPage = 0;
-	String currentPageStr = request.getParameter("currentPage");
-	if(currentPageStr == null) {
-		currentPage = 1;
-	} else {
-		currentPage = Integer.parseInt(currentPageStr);
-	}
-	
-	BoardService service = BoardService.getInstance();
-	ArticlePageVO articlePage = service.createArticlePage(currentPage);
-%>
 <table border = "1">
 	<tr>
 		<th>글번호</th>
@@ -31,16 +21,15 @@
 		<th>개제시간</th>
 		<th>조회수</th>
 	</tr>
-	<%for(ArticleVO article : articlePage.getArticleList()) {%>
-	<tr>
-		<td><%=article.getArticleId() %></td>
-		<td><%=article.getWriter() %></td>
-		<td><%=article.getTitle() %></td>
-		<td><%=article.getWriteDate() %></td>
-		<td><%=article.getReadCount() %></td>
-	</tr>
-	<%} %>
+	<c:forEach items="${requestScope.articlePage.articleList}" var="article">
+		<tr>
+			<td>${article.articleId}</td>
+			<td>${article.writer}</td>
+			<td>${article.title}</td>
+			<td>${article.writeDate}</td>
+			<td>${article.readCount}</td>
+		</tr>
+	</c:forEach>
 </table>
-<% for(int i=articlePage.getStartPage(); i<=articlePage.getEndPage(); i++) {%> [<%=i %>] <%} %> 
 </body>
 </html>
---
+++ BootstrapBoard05/src/servlet/BoardController.java
@@ -0,0 +1,52 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.BoardService;
+import vo.ArticlePageVO;
+
+@WebServlet("/board.do")
+public class BoardController extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		execute(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("euc-kr");
+		execute(request, response);
+	}
+	
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		String viewPath = "";
+		
+		if(action == null || action.equals("main")) {
+			int currentPage = 0;
+			String currentPageStr = request.getParameter("currentPage");
+			if(currentPageStr == null) {
+				currentPage = 1;
+			} else {
+				currentPage = Integer.parseInt(currentPageStr);
+			}
+			
+			BoardService service = BoardService.getInstance();
+			ArticlePageVO articlePage = service.createArticlePage(currentPage);
+			
+			request.setAttribute("articlePage", articlePage);
+			
+			viewPath="board_list.jsp";
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+		dispatcher.forward(request, response);
+	}
+}
